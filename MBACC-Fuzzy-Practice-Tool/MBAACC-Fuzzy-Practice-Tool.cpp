09d82bdf845bedae82c124208ef9f68db689def1
Will Hildreth <willhildreth96@gmail.com>
2024-04-15 00:25:42 -0500

    random isn't working very well, but you're dumb so that makes sense

--- MBACC-Fuzzy-Practice-Tool/MBAACC-Fuzzy-Practice-Tool.cpp
+++ MBACC-Fuzzy-Practice-Tool/MBAACC-Fuzzy-Practice-Tool.cpp
@@ -4,6 +4,14 @@
 #include <string.h>
 #include <psapi.h>
 #include <iostream>
+#include <ctime>
+#include <vector>
+
+enum eEnemyStatus { STAND, JUMP, CROUCH, CPU, MANUAL, DUMMY };
+enum eEnemyDefense { NOGUARD, ALLGUARD, STATUSGUARD, ALLSHIELD, STATUSSHIELD, DODGE };
+enum eEnemyStance { STANDING = 0, STANDGUARDING = 17, CROUCHING = 13 };
+enum ePresetSettings { FUZZY, HITCONFIRM, CUSTOM };
+enum eEnemyDefenseBiasSetting { UNLIKELY, EVEN, LIKELY, OFF };
 
 HANDLE GetProcessByName(const wchar_t* name)
 {
@@ -65,6 +73,27 @@ DWORD GetBaseAddressByName(HANDLE pHandle, const wchar_t* name)
     return 0;
 }
 
+void ApplyPreset(int nPresetSetting, bool* bSwitchToCrouch, int* nDelayFrames, int* nEnemyStatusSetting, int* nEnemyDefenseSetting, int* nEnemyDefenseBiasSetting)
+{
+    switch (nPresetSetting)
+    {
+    case ePresetSettings::FUZZY:
+        *bSwitchToCrouch = true;
+        *nDelayFrames = 0;
+        *nEnemyStatusSetting = eEnemyStatus::STAND;
+        *nEnemyDefenseSetting = eEnemyDefense::STATUSGUARD;
+        *nEnemyDefenseBiasSetting = eEnemyDefenseBiasSetting::OFF;
+        break;
+    case ePresetSettings::HITCONFIRM:
+        *bSwitchToCrouch = false;
+        *nDelayFrames = 0;
+        *nEnemyStatusSetting = eEnemyStatus::CROUCH;
+        *nEnemyDefenseSetting = eEnemyDefense::ALLGUARD;
+        *nEnemyDefenseBiasSetting = eEnemyDefenseBiasSetting::LIKELY;
+        break;
+    }
+}
+
 int main(int argc, char* argv[])
 {
     HANDLE hProcess = 0x0;
@@ -78,16 +107,32 @@ int main(int argc, char* argv[])
     DWORD dwEnemyStatus = 0x37C1E8; //0:STAND 1:JUMP 2:CROUCH 3:CPU 4:MANUAL 5:DUMMY
     DWORD dwEnemyDefense = 0x37C1F0; //0:OFF 1:ALLGUARD 2:STATUSGUARD 3:ALLSHIELD 4:STATUSSHIELD 5:DODGE
 
-    enum eEnemyStatus { STAND, JUMP, CROUCH, CPU, MANUAL, DUMMY };
-    enum eEnemyDefense { OFF, ALLGUARD, STATUSGUARD, ALLSHIELD, STATUSSHIELD, DODGE };
-    enum eEnemyStance { STANDING = 0, STANDGUARDING = 17, CROUCHING = 13 };
-
     int nReadResult;
     int nWriteBuffer;
     int nDelayFrames = 0;
 
     bool bUpPressed = false;
     bool bDownPressed = false;
+    bool bRightPressed = false;
+    bool bLeftPressed = false;
+
+    bool bSwitchToCrouch = false;
+    bool bRandomBlock = false;
+    int nPresetSetting = ePresetSettings::CUSTOM;
+    int nEnemyDefenseSetting = eEnemyDefense::NOGUARD;
+    int nEnemyDefenseBiasSetting = eEnemyDefenseBiasSetting::OFF;
+    int nEnemyStatusSetting = eEnemyStatus::STAND;
+    std::vector<std::string> vPresetSettings = { "Fuzzy Overhead", "Hitconfirming from blockstring", "Custom" };
+    std::vector<std::string> vEnemyDefenseSettings = { "No Guard", "All Guard", "Status Guard", "All Shield", "Status Shield"};
+    std::vector<std::string> vEnemyDefenseBiasSettings = { "Unlikely", "Even", "Likely", "Off" };
+    std::vector<std::string> vEnemyStatusSettings = { "Stand", "Jump", "Crouch" };
+
+    int nCursorIndex = 0;
+
+    int nDebugBias = 0;
+    int nDebugFrameCount = 0;
+
+    std::srand(std::time(nullptr));
 
     static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_CURSOR_INFO cursorInfo;
@@ -96,26 +141,38 @@ int main(int argc, char* argv[])
     SetConsoleCursorInfo(hOut, &cursorInfo);
 
     system("cls");
-    std::cout << "Fang's Fuzzy Practice Tool v1.0" << std::endl;
+    std::cout << "Fang's Extended Training Mode v1.0" << std::endl;
 
     while (1)
     {
-        nWriteBuffer = eEnemyStatus::STAND;
-        WriteProcessMemory(hProcess, (LPVOID)(dwBaseAddress + dwEnemyStatus), &nWriteBuffer, 4, 0);
-
-        nWriteBuffer = eEnemyDefense::ALLGUARD;
-        WriteProcessMemory(hProcess, (LPVOID)(dwBaseAddress + dwEnemyDefense), &nWriteBuffer, 4, 0);
-
+        // Loop till the dummy is blocking
         while (1)
         {
-            ReadProcessMemory(hProcess, (LPVOID)(dwBaseAddress + dwPlayerState + dwP2Offset), &nReadResult, 4, 0);
-            if (nReadResult == eEnemyStance::STANDGUARDING)
-                break;
+            Sleep(16);
+            nDebugFrameCount++;
 
-            //system("cls");
             std::cout.flush();
+
+            SetConsoleCursorPosition(hOut, { 0, 2 });
+            std::cout << (nCursorIndex == 0 ? "=>  " : "    ") << "Preset: <- " << vPresetSettings[nPresetSetting] << " ->                                        ";
+
+            SetConsoleCursorPosition(hOut, { 0, 4 });
+            std::cout << (nCursorIndex == 1 ? "=>  " : "    ") << "Switch to crouching after blocking: <- " << (bSwitchToCrouch ? "On " : "Off") << " ->      ";
+
             SetConsoleCursorPosition(hOut, { 0, 5 });
-            std::cout << "Delay before crouching: " << nDelayFrames << "              ";
+            std::cout << (nCursorIndex == 2 ? "=>  " : "    ") << "Delay before crouching: <- " << nDelayFrames << " ->              ";
+
+            SetConsoleCursorPosition(hOut, { 0, 7 });
+            std::cout << (nCursorIndex == 3 ? "=>  " : "    ") << "Enemy Status: <- " << vEnemyStatusSettings[nEnemyStatusSetting] << " ->              ";
+
+            SetConsoleCursorPosition(hOut, { 0, 9 });
+            std::cout << (nCursorIndex == 4 ? "=>  " : "    ") << "Enemy Defense: <- " << vEnemyDefenseSettings[nEnemyDefenseSetting] << " ->              ";
+
+            SetConsoleCursorPosition(hOut, { 0, 10 });
+            std::cout << (nCursorIndex == 5 ? "=>  " : "    ") << "Random Bias: <- " << vEnemyDefenseBiasSettings[nEnemyDefenseBiasSetting] << " ->              ";
+
+            SetConsoleCursorPosition(hOut, { 0, 11 });
+            std::cout << "Bias:" << nDebugBias << " FrameCount:" << nDebugFrameCount;
 
             if (GetAsyncKeyState(VK_ESCAPE))
                 goto CLEANUP;
@@ -124,7 +181,7 @@ int main(int argc, char* argv[])
             {
                 if (!bUpPressed)
                 {
-                    nDelayFrames++;
+                    nCursorIndex = max(0, nCursorIndex - 1);
                     bUpPressed = true;
                 }
             }
@@ -135,31 +192,136 @@ int main(int argc, char* argv[])
             {
                 if (!bDownPressed)
                 {
-                    nDelayFrames = max(0, nDelayFrames - 1);
+                    nCursorIndex++;
                     bDownPressed = true;
                 }
             }
             else
                 bDownPressed = false;
 
-        }
+            
+            if (GetAsyncKeyState(VK_RIGHT))
+            {
+                if (!bRightPressed)
+                {
+                    bRightPressed = true;
+                    if (nCursorIndex != 0)
+                        nPresetSetting = ePresetSettings::CUSTOM;
 
-        Sleep(16 * nDelayFrames);
+                    switch (nCursorIndex)
+                    {
+                    case 0:
+                        nPresetSetting = min(nPresetSetting + 1, vPresetSettings.size() - 2);
+                        ApplyPreset(nPresetSetting, &bSwitchToCrouch, &nDelayFrames, &nEnemyStatusSetting, &nEnemyDefenseSetting, &nEnemyDefenseBiasSetting);
+                        break;
+                    case 1:
+                        bSwitchToCrouch = !bSwitchToCrouch;
+                        break;
+                    case 2:
+                        nDelayFrames++;
+                        break;
+                    case 3:
+                        nEnemyStatusSetting = min(nEnemyStatusSetting + 1, vEnemyStatusSettings.size() - 1);
+                        break;
+                    case 4:
+                        nEnemyDefenseSetting = min(nEnemyDefenseSetting + 1, vEnemyDefenseSettings.size() - 1);
+                        break;
+                    case 5:
+                        nEnemyDefenseBiasSetting = max(nEnemyDefenseBiasSetting - 1, 0);
+                        break;
+                    default:
+                        break;
+                    }
+                }
+            }
+            else
+                bRightPressed = false;
 
-        nWriteBuffer = eEnemyStatus::CROUCH;
-        WriteProcessMemory(hProcess, (LPVOID)(dwBaseAddress + dwEnemyStatus), &nWriteBuffer, 4, 0);
+            if (GetAsyncKeyState(VK_LEFT))
+            {
+                if (!bLeftPressed)
+                {
+                    bLeftPressed = true;
+                    if (nCursorIndex != 0)
+                        nPresetSetting = ePresetSettings::CUSTOM;
 
-        nWriteBuffer = eEnemyDefense::STATUSGUARD;
-        WriteProcessMemory(hProcess, (LPVOID)(dwBaseAddress + dwEnemyDefense), &nWriteBuffer, 4, 0);
+                    switch (nCursorIndex)
+                    {
+                    case 0:
+                        nPresetSetting = max(nPresetSetting - 1, 0);
+                        ApplyPreset(nPresetSetting, &bSwitchToCrouch, &nDelayFrames, &nEnemyStatusSetting, &nEnemyDefenseSetting, &nEnemyDefenseBiasSetting);
+                        break;
+                    case 1:
+                        bSwitchToCrouch = !bSwitchToCrouch;
+                        break;
+                    case 2:
+                        nDelayFrames = max(0, nDelayFrames - 1);
+                        break;
+                    case 3:
+                        nEnemyStatusSetting = max(nEnemyStatusSetting - 1, 0);
+                        break;
+                    case 4:
+                        nEnemyDefenseSetting = max(nEnemyDefenseSetting - 1, 0);
+                        break;
+                    case 5:
+                        nEnemyDefenseBiasSetting = min(nEnemyDefenseBiasSetting + 1, vEnemyDefenseBiasSettings.size() - 1);
+                        break;
+                    default:
+                        break;
+                    }
+                }
+            }
+            else
+                bLeftPressed = false;
 
-        while (1)
-        {
+
+            nWriteBuffer = nEnemyStatusSetting;
+            WriteProcessMemory(hProcess, (LPVOID)(dwBaseAddress + dwEnemyStatus), &nWriteBuffer, 4, 0);
+
+            int nRandVal = std::rand() % 4;
+            if (nEnemyDefenseBiasSetting != eEnemyDefenseBiasSetting::OFF && nDebugFrameCount % 10 == 0)
+            {
+                if (nRandVal < nEnemyDefenseBiasSetting + 1)
+                {
+                    nDebugBias++;
+                    nWriteBuffer = nEnemyDefenseSetting;
+                    WriteProcessMemory(hProcess, (LPVOID)(dwBaseAddress + dwEnemyDefense), &nWriteBuffer, 4, 0);
+                }
+                else
+                {
+                    nDebugBias--;
+                    nWriteBuffer = eEnemyDefense::NOGUARD;
+                    WriteProcessMemory(hProcess, (LPVOID)(dwBaseAddress + dwEnemyDefense), &nWriteBuffer, 4, 0);
+                }
+            }
+            else
+            {
+                
+            }
+
             ReadProcessMemory(hProcess, (LPVOID)(dwBaseAddress + dwPlayerState + dwP2Offset), &nReadResult, 4, 0);
-            if (nReadResult == eEnemyStance::CROUCHING)
+            if (nReadResult == eEnemyStance::STANDGUARDING)
                 break;
         }
 
-        
+        if (bSwitchToCrouch)
+        {
+            Sleep(16 * nDelayFrames);
+
+            nWriteBuffer = eEnemyStatus::CROUCH;
+            WriteProcessMemory(hProcess, (LPVOID)(dwBaseAddress + dwEnemyStatus), &nWriteBuffer, 4, 0);
+
+            nWriteBuffer = eEnemyDefense::STATUSGUARD;
+            WriteProcessMemory(hProcess, (LPVOID)(dwBaseAddress + dwEnemyDefense), &nWriteBuffer, 4, 0);
+
+            // Loop until the dummy returns to neutral
+            while (1)
+            {
+                ReadProcessMemory(hProcess, (LPVOID)(dwBaseAddress + dwPlayerState + dwP2Offset), &nReadResult, 4, 0);
+                if (nReadResult == eEnemyStance::CROUCHING)
+                    break;
+            }
+        }
     }
 
 CLEANUP:
