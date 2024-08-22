c13d10e9cfa88b6e3bf3527008e8e8f0a46cfb9e
Will Hildreth <willhildreth96@gmail.com>
2024-08-22 13:07:11 -0500

    Update MBAACC-Extended-Training-Mode.h

--- MBAACC-Extended-Training-Mode/MBAACC-Extended-Training-Mode.h
+++ MBAACC-Extended-Training-Mode/MBAACC-Extended-Training-Mode.h
@@ -270,6 +270,28 @@ std::wstring GetFilePath()
     GetOpenFileNameW(&ofn);
 
     return std::wstring(ofn.lpstrFile);
+
+    /*
+    // use this to process errors if we need to at some point
+    switch (CommDlgExtendedError())
+    {
+      case CDERR_DIALOGFAILURE   : std::cout << "CDERR_DIALOGFAILURE\n";   break;
+      case CDERR_FINDRESFAILURE  : std::cout << "CDERR_FINDRESFAILURE\n";  break;
+      case CDERR_INITIALIZATION  : std::cout << "CDERR_INITIALIZATION\n";  break;
+      case CDERR_LOADRESFAILURE  : std::cout << "CDERR_LOADRESFAILURE\n";  break;
+      case CDERR_LOADSTRFAILURE  : std::cout << "CDERR_LOADSTRFAILURE\n";  break;
+      case CDERR_LOCKRESFAILURE  : std::cout << "CDERR_LOCKRESFAILURE\n";  break;
+      case CDERR_MEMALLOCFAILURE : std::cout << "CDERR_MEMALLOCFAILURE\n"; break;
+      case CDERR_MEMLOCKFAILURE  : std::cout << "CDERR_MEMLOCKFAILURE\n";  break;
+      case CDERR_NOHINSTANCE     : std::cout << "CDERR_NOHINSTANCE\n";     break;
+      case CDERR_NOHOOK          : std::cout << "CDERR_NOHOOK\n";          break;
+      case CDERR_NOTEMPLATE      : std::cout << "CDERR_NOTEMPLATE\n";      break;
+      case CDERR_STRUCTSIZE      : std::cout << "CDERR_STRUCTSIZE\n";      break;
+      case FNERR_BUFFERTOOSMALL  : std::cout << "FNERR_BUFFERTOOSMALL\n";  break;
+      case FNERR_INVALIDFILENAME : std::cout << "FNERR_INVALIDFILENAME\n"; break;
+      case FNERR_SUBCLASSFAILURE : std::cout << "FNERR_SUBCLASSFAILURE\n"; break;
+      default                    : std::cout << "You cancelled.\n";
+    }*/
 }
 
 
