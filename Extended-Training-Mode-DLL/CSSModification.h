e6a4f7b9c11fb3cb69c9782898f3bd311d5b8648
Meepster99 <20229468+Meepster99@users.noreply.github.com>
2024-09-02 14:37:58 -0400

    sync

--- Extended-Training-Mode-DLL/CSSModification.h
+++ Extended-Training-Mode-DLL/CSSModification.h
@@ -264,8 +264,11 @@ void loadTextureFromPathUnknown() {
 
 }
 
-DWORD _naked_PalettePatcherHook_ebx;
-DWORD _naked_PalettePatcherCallback_eax;
+DWORD ougi_warc[256] = { 0x00000000, 0x00F5ECDF, 0x00D3C4BE, 0x00C8928E, 0x00945E5A, 0x004D4744, 0x00393931, 0x00292921, 0x00181810, 0x00F7C0C0, 0x00BB7790, 0x00AB587D, 0x007D3F5B, 0x00484854, 0x0030303A, 0x00181820, 0x00F9E6D3, 0x000A0A0A, 0x00FF0000, 0x009C0000, 0x009C6600, 0x00FFEE00, 0x0064555F, 0x001F1C21, 0x00292129, 0x00EAE7EA, 0x00766C8D, 0x002B244F, 0x00FFFFC3, 0x00FFDC2B, 0x00D79B21, 0x00FCF2E9, 0x00F6DABD, 0x00080910, 0x000E0E0E, 0x000E0F1E, 0x00141736, 0x0016172D, 0x001F1F1F, 0x00191C47, 0x002A2A36, 0x00212769, 0x0026295E, 0x00303030, 0x002F3051, 0x00292F82, 0x003D3D3D, 0x0043444F, 0x003F4072, 0x003B3C84, 0x00353CA4, 0x00505050, 0x00434699, 0x004E4F68, 0x0054568E, 0x005658A0, 0x00636363, 0x0061616D, 0x005658BB, 0x005D5FB4, 0x006A6B86, 0x00717171, 0x006C6D99, 0x006568DE, 0x00F3CEA7, 0x00FFFFFF, 0x00FFFFFF, 0x00C9CADB, 0x0094967A, 0x00565747, 0x00F8FADE, 0x00BABBAB, 0x00E8EAC2, 0x00FFFFFC, 0x00BEBC98, 0x0072715C, 0x00D6D5C0, 0x001A1914, 0x007E7D78, 0x002E2C23, 0x00929087, 0x00423D31, 0x00A6A195, 0x006E6E6E, 0x000A0A0A, 0x005D6095, 0x00261809, 0x004C3013, 0x0072471D, 0x00986027, 0x00BE7731, 0x00E4913B, 0x00E79C50, 0x00EAA966, 0x00EDB57C, 0x00F0C192, 0x009395B8, 0x00090006, 0x000F000B, 0x001D000B, 0x000C090D, 0x00170316, 0x0020011F, 0x002D0113, 0x00290127, 0x00220C20, 0x00330130, 0x003C0217, 0x002C0927, 0x00191719, 0x004D011B, 0x003F023D, 0x002A1328, 0x005F0118, 0x004E024B, 0x00420D3C, 0x00391434, 0x002A2129, 0x00580254, 0x00650631, 0x00730122, 0x00520E4C, 0x00650162, 0x00431C3F, 0x00630B5F, 0x00850331, 0x00302F30, 0x00750371, 0x00581751, 0x0052234F, 0x00990339, 0x006E1167, 0x00850380, 0x003F353E, 0x00761C3B, 0x006E1A6A, 0x00B20141, 0x0084127B, 0x00960490, 0x00573250, 0x00652862, 0x00A80B4E, 0x007D1F79, 0x00464546, 0x00C7024E, 0x00961E46, 0x00AC06A2, 0x0099158F, 0x0072316F, 0x0062405E, 0x00DE035D, 0x008A2686, 0x008C3353, 0x00BD07B5, 0x00A817A3, 0x00535353, 0x00BD2159, 0x0072466F, 0x00F1086E, 0x008B3788, 0x00A6269A, 0x00CF0AC5, 0x00605E60, 0x00A0349B, 0x00DA226B, 0x00B429AD, 0x007E4F79, 0x00D912D4, 0x00C720C1, 0x00974494, 0x00B73D66, 0x00FB1982, 0x00AC3EA8, 0x008D5987, 0x00CD32C3, 0x00727072, 0x00E521DF, 0x00EB3580, 0x00A652A4, 0x00BE44B9, 0x00C84E79, 0x00946891, 0x00D740CE, 0x00B65AB3, 0x00838283, 0x00EF4A8F, 0x00EF39EA, 0x00D65E89, 0x00A0779C, 0x00D552CD, 0x00BD6CB9, 0x00F34AF0, 0x00918F91, 0x00E15AD8, 0x00ED659C, 0x00DA6AD5, 0x00F459F1, 0x00BB82B8, 0x009E9D9E, 0x00ED76A6, 0x00EF6AE9, 0x00DA7CD6, 0x00C595C2, 0x00F283B0, 0x00ABAAAA, 0x00F179EC, 0x00DE8DDB, 0x00F591BB, 0x00F48AF0, 0x00B7B4B8, 0x00DCA0D9, 0x00F697F1, 0x00CABEC9, 0x00FAA6CA, 0x00F2A7EF, 0x00F0B5ED, 0x00D0CFD0, 0x00FABCE9, 0x00E8CBE8, 0x00E0DCE1, 0x00FBCEF4, 0x00EFE3EE, 0x00F4EEF4, 0x00FDFDFD, 0x007475AD, 0x007273BD, 0x007D7E80, 0x007876CE, 0x0083839D, 0x007B7AE0, 0x008A8A8A, 0x008A8AB5, 0x008989D8, 0x00908FCF, 0x00999999, 0x009999A9, 0x009B9BBD, 0x009C9AEE, 0x00A8A8A8, 0x00A0A1D5, 0x00A8A8B3, 0x00B0B2CE, 0x00ADACF1, 0x00B3B4E1, 0x00BABABB, 0x00B6B6F0, 0x00C1C2DE, 0x00C2C2F2, 0x00CCCCCC, 0x00CAC9EF, 0x00CFD0D9, 0x00D3D2EF, 0x00DFDFDF, 0x00DEDEF6, 0x00E7E6F3, 0x00F6F6F9 };
+
+
+DWORD _naked_PalettePatcherHook_ebx = 0;
+DWORD _naked_PalettePatcherCallback_eax = 0;
 void palettePatcherCallback() {
 	// palette loads take a different route. this route.
 
@@ -290,13 +293,40 @@ void palettePatcherCallback() {
 
 	const char* fileType = loadString + len - 3;
 
+	const char* tempStringPtr = loadString + len - 1;
+
+	const char* fileName = NULL;
+
+	while (tempStringPtr > loadString) {
+		if (*tempStringPtr == '\\') {
+			fileName = tempStringPtr+1;
+			break;
+		}
+		tempStringPtr--;
+	}
+
+	if (fileName == NULL) {
+		return;
+	}
+
 	// P_ARC_D gave a crash? what evem is that
 
 	if (strcmp(fileType, "pal")) { // not a palette, do nothing
 		return;
 	}
 
+	if (!strcmp(fileName, "P_ARC_D.pal")) {
+		log("bailing out on weird hime thing");
+		return;
+	}
+
 	// is a palette
+
+	bool isWarc = false;
+
+	if (!strcmp(fileName, "WARC.pal")) {
+		isWarc = true;
+	}
 
 	// textures, when loaded in, are in 4 byte RGBA format.
 	// weirdly, there is a 40 00 00 00 at the start. what is that?
@@ -308,11 +338,20 @@ void palettePatcherCallback() {
 	//for (int i = 0; i < 64; i++) {
 	for (int i = 36; i < 42; i++) {
 		for (int j = 0; j < 256; j++) {
-
+			
 			DWORD tempColor = colors[(256 * i) + j];
 
 			BYTE a = (tempColor & 0xFF000000) >> 24;
 
+			
+			if (isWarc && i == 36) {
+				//colors[(256 * i) + j] = (a << 24) | ougi_warc[j];
+				//colors[(256 * i) + j] = (a << 24) | (0xFF80);
+				continue;
+			}
+			
+			
+			
 			D3DXVECTOR3 rgb = D3DXVECTOR3(
 				(float)((tempColor & 0x00FF0000) >> 16),
 				(float)((tempColor & 0x0000FF00) >> 8),
@@ -321,7 +360,22 @@ void palettePatcherCallback() {
 
 			D3DXVECTOR3 hsv = RGBtoHSV(rgb);
 
-			hsv.x = 0.63;
+
+			switch (i % 3) {
+			default:
+			case 0:
+				hsv.x = 0.0f;
+				break;
+			case 1:
+				hsv.x = 120.0f;
+				break;
+			case 2:
+				hsv.x = 240.0f;
+				break;
+			}
+			
+
+			//log("h: %5.2f s: %5.2f v: %5.2f", hsv.x, hsv.y, hsv.z);
 
 			D3DXVECTOR3 tempRGB = HSVtoRGB(hsv);
 
@@ -329,9 +383,15 @@ void palettePatcherCallback() {
 			BYTE g = tempRGB.y;
 			BYTE b = tempRGB.z;
 
+			//g = 0xF0;
+
 			DWORD tempNewColor = (a << 24) | (r << 16) | (g << 8) | (b << 0);
 
+			//if (j >= 16) {
+			//	continue;
+			//}
 			colors[(256 * i) + j] = tempNewColor;
+
 		}		
 	}
 }
