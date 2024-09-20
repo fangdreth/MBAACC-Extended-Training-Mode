0870790d27f3281ea97057aaa55971c0c7a7e271
Meepster99 <20229468+Meepster99@users.noreply.github.com>
2024-09-20 11:10:47 -0400

    pruned logserver

--- logServer.py
+++ logServer.py
@@ -35,7 +35,6 @@ def server(packetQueue):
 					except:
 						packetQueue.put(f"{data}")
 						continue
-					#if d != "0":
 					packetQueue.put(f"{d}")
 			except socket.timeout: # only here so ctrl c works
 				time.sleep(1)
@@ -54,27 +53,10 @@ def writerFunc(packetQueue):
 
 		if data is None:
 			break
-		print(f'got data: {data}')
+		print(f'{data}')
 
-		#if len(data) == 10:
-		#	char, d = data.split(" ")
-		#	if char == "0":
-		#		pointerSet1.add(int(d, 16))
-		#	else:
-		#		pointerSet2.add(int(d, 16))
-		# not ideal, but works
-		#with open("MBAACC.log", "a") as f:
-		#	f.write(f"{data}\n")
 		pass
 	
-	#with open("MBAACC1.log", "a") as f:
-	#	for ptr in pointerSet1:
-	#		f.write(f"{ptr:08X}\n")
-	#		
-	#with open("MBAACC2.log", "a") as f:
-	#	for ptr in pointerSet2:
-	#		f.write(f"{ptr:08X}\n")
-		
 def main():
 	packetQueue = queue.Queue() # queues are thread safe by default in python
 
@@ -88,42 +70,5 @@ def main():
 	print("waiting on writer to join")
 	writerThread.join()
 
-def graph():
-
-	vals = []
-
-	for filename in ["MBAACC1.log", "MBAACC2.log"]:
-		with open(filename, "r") as f:
-			for l in [ l.strip() for l in f.readlines() if len(l.strip()) != 0]:
-				vals.append(int(l, 16))
-
-		vals.sort()
-
-		prev = vals[0]
-		
-		for v in vals[1:]:
-			print(f"{v - prev:08X}")
-			prev = v
-
-		print(f"start addr was {vals[0]:08X}")
-		print(f"end   addr was {vals[-1]:08X}")
-
-		# typical offset bounds of 0x54
-		# is base(starting) val gotten on css?
-		
-		# 04D0BF68
-		# 0047961b
-		
-		# 04B628F0
-		# 04D35B38
-		
-		# start addr was 04B7E9B0
-		# end   addr was 04D9832C
-		# start addr was 04B7E9B0
-		# end   addr was 19871E84
-		
-	pass
-	
 if __name__ == '__main__':
 	main()
-	#graph()
