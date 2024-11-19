8f58d23114f52d2bc4a087e2250f62cc283514fc
Meepster99 <20229468+Meepster99@users.noreply.github.com>
2024-11-19 01:09:59 -0500

    delete version tracking files

--- Common/version.cpp
+++
@@ -1,5 +0,0 @@
-#include "version.h" 
-const char* GIT_HASH = ""; 
-const char* GIT_DIRTY = "D"; 
-const char* GIT_VERSION = "_D"; 
-const bool BLEEDING = false; 
--- Extended-Training-Mode-DLL/version.h
+++
@@ -1,4 +0,0 @@
-#pragma once 
-#define GIT_HASH "" 
-#define GIT_DIRTY "D" 
-#define GIT_VERSION "_D" 
