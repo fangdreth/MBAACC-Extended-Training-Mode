bec5fab517530a6731af43710b0d6fd00e475be8
Meepster99 <20229468+Meepster99@users.noreply.github.com>
2026-08-25 15:42:36 -0400

    expose a function to give caster the etm version

--- Common/genVersion.bat
+++ Common/genVersion.bat
@@ -13,12 +13,22 @@ echo const char* GIT_HASH = "%GIT_HASH%"; >> version.cpp
 echo const char* GIT_DIRTY = "%GIT_DIRTY%"; >> version.cpp
 echo const char* GIT_VERSION = "%GIT_HASH%_%GIT_DIRTY%"; >> version.cpp
 
+
 if "%BLEEDING%"=="^%BLEEDING^%" (
+	set BLOOD=F
 	echo const bool BLEEDING = false; >> version.cpp
 ) else (
 	if "%BLEEDING%"=="" (
+		set BLOOD=F
 		echo const bool BLEEDING = false; >> version.cpp
 	) else (
+		set BLOOD=T
 		echo const bool BLEEDING = true; >> version.cpp
 	)
 )
+
+echo const char* GIT_FULL_VERSION = "%GIT_HASH%_%GIT_DIRTY%_%BLOOD%"; >> version.cpp
+
+echo const char* getETMVersion() { >> version.cpp
+echo return GIT_FULL_VERSION; >> version.cpp
+echo } >> version.cpp
--- Common/version.h
+++ Common/version.h
@@ -4,3 +4,6 @@ extern const char* GIT_HASH;
 extern const char* GIT_DIRTY;
 extern const char* GIT_VERSION;
 extern const bool BLEEDING;
+extern const char* GIT_FULL_VERSION;
+
+extern "C" __declspec(dllexport) const char* getETMVersion();
